c5391f6ba1a2841d8cba52c751c67bd5cec9460b
McKay <kg89szeged@gmail.com>
2023-09-23 21:33:44 +0200

    [ ADD ] Main.jsx created with test fetching

---
+++ client/pokemon/src/components/Main.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import { Outlet, Link } from "react-router-dom";
+
+export default function Main() {
+    const [testData, setTestData] = useState({});
+
+    useEffect(() => {
+      const fetchTestData = async () => {
+        try {
+          const response = await fetch('/api/testdata');
+          const data = await response.json();
+          setTestData(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      fetchTestData();
+    }, [])
+    
+
+    return (
+      <>
+        <div>
+            {testData.name}
+        </div>
+        <Outlet /> 
+      </>
+    )
+}
