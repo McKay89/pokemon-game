f215ad873247d35e3d52daf71115049c0cae1d7b
McKay <kg89szeged@gmail.com>
2023-10-08 19:40:45 +0200

    [ UPDATE ] Routes was added, login data handler added

--- client/pokemon/src/App.jsx
+++ client/pokemon/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom';
 import axios from "axios";
 import { useTranslation } from "react-i18next";
@@ -10,6 +10,8 @@ import translationHU from "./locales/hu/translation.json";
 import Main from './components/Main';
 import Info from './components/Info';
 import Navbar from './components/Navbar';
+import TestDownload from './components/pages/TestDownload';
+import BackgroundCover from './components/BackgroundCover';
 import './App.css'
 
 const resources = {
@@ -31,15 +33,72 @@ i18n.use(initReactI18next).init({
 });
 
 function App() {
+  const userDataJSON = localStorage.getItem('userData');
   const [user, setUser] = useState("")
   const { t } = useTranslation();
+  const [sidebarHovered, setSidebarHovered] = useState(true);
+  const [userData, setUserData] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleSidebarHover = (value) => {
+    setSidebarHovered(value);
+  }
+
+  const fillUserData = (data) => {
+    setLoggedIn(true);
+    localStorage.setItem('userData', JSON.stringify(data));
+  }
+
+  const handleLogout = () => {
+    setUserData({logged: false});
+    setLoggedIn(false);
+    localStorage.removeItem('userData');
+  }
+
+  useEffect(() => {
+    if(userDataJSON) {
+      setUserData(JSON.parse(userDataJSON));
+    }
+  }, [loggedIn])
+
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+  };
+  
   return ( 
-    <Routes>
-      <Route path='/' element={<Navbar translation={t} />}>
-        <Route path='/' element={<Main translation={t} />} />
-        <Route path='/info' element={<Info translation={t}/>} />
-      </Route>
-    </Routes>
+    <div className="app-container" onContextMenu={handleContextMenu}>
+      <div className="app-navbar">
+        <Navbar
+          translation={t}
+          handleSidebarHover={handleSidebarHover}
+          user={userData}
+          login={fillUserData}
+          handleLogout={handleLogout}
+        />
+      </div>
+      <BackgroundCover />
+      <div className="app-components">
+        <Routes>
+          <Route path='/' element= {
+            <Main
+              translation={t}
+              sidebarHovered={sidebarHovered}
+            />}
+          />
+          <Route path='/info' element={
+            <Info
+              translation={t}
+            />}
+          />
+          <Route path='/testdownload' element={
+            <TestDownload
+              translation={t}
+              sidebarHovered={sidebarHovered}
+            />}
+          />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
