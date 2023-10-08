bbc08a6c0615dc31a9bc7b3d7c0460ca17d599d3
McKay <kg89szeged@gmail.com>
2023-10-08 19:44:09 +0200

    [ UPDATE ] logo image added

--- client/pokemon/src/components/Main.jsx
+++ client/pokemon/src/components/Main.jsx
@@ -1,8 +1,9 @@
 import i18next from 'i18next';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet, Link } from "react-router-dom";
+import "../styles/Main.css";
 
-export default function Main({translation}) {
+export default function Main({translation, sidebarHovered}) {
     const [testData, setTestData] = useState({});
     const [gameLanguage, setGameLanguage] = useState(i18next.language);
 
@@ -19,14 +20,18 @@ export default function Main({translation}) {
       }
 
       fetchTestData();
-    }, [i18next.language])
-    
+    }, [i18next.language])    
 
     return (
       <>
-        <div>
-            <span>Name: {testData.name}</span><br />
-            <span>Description: {testData.desc && testData.desc[gameLanguage]}</span>
+        <div className={sidebarHovered ? 'home-container-hovered' : 'home-container'}>
+          <img className="home-logo-img" src="/images/logo/home_logo_3.png" />
+          <div className="mckay89">
+            <span>Pokemon - The Card Game</span><br />
+            <span>Developed by McKay89</span><br />
+            <span>Version: 0.1 (development phase)</span><br />
+            <span>Last update: 2023.10.07</span>
+          </div>
         </div>
         <Outlet /> 
       </>
