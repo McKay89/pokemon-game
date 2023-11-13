8711d7045435cd515c56f13914546a5a03f9bc42
McKay <kg89szeged@gmail.com>
2023-11-13 15:02:30 +0100

    [ UPDATE ] type and animation props added

--- client/src/components/adventure/classes/Sprite.js
+++ client/src/components/adventure/classes/Sprite.js
@@ -1,9 +1,10 @@
 class Sprite {
-    constructor({ position, velocity, image, frames = { max: 1 }, sprites, player = { height: 1, width: 1 } }) {
+    constructor({ position, velocity, image, frames = { max: 1 }, sprites, object = { height: 1, width: 1 }, type = "object", animation = { speed: 1 } }) {
         this.position = position
         this.image = image
         this.frames = {...frames, val: 0, elapsed: 0}
-        this.player = player
+        this.object = object
+        this.type = type
 
         this.image.onload = () => {
             this.width = this.image.width / this.frames.max
@@ -11,6 +12,7 @@ class Sprite {
         }
         this.moving = false
         this.sprites = sprites
+        this.animation = animation
     }
 
     draw(c) {
@@ -22,18 +24,18 @@ class Sprite {
             this.image.height,
             this.position.x,
             this.position.y,
-            this.image.width / this.player.width,
-            this.image.height * this.player.height
+            this.image.width / this.object.width,
+            this.image.height * this.object.height
         )
 
-        if(this.moving) {
+        if(this.moving && this.type == "player" || this.type == "object") {
             if(this.frames.max > 1) {
                 this.frames.elapsed++
             }
     
-            if(this.frames.elapsed % 8 === 0) {
+            if(this.frames.elapsed % this.animation.speed === 0) {
                 if(this.frames.val < this.frames.max - 1) this.frames.val++
-                else this.frames.val = 0
+                else this.frames.val = 0    
             }
         } else {
             this.frames.val = 0
