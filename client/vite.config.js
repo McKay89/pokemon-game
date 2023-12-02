ee643da915c4cd33d54b6edf195795b3ed712372
McKay <kg89szeged@gmail.com>
2023-12-02 13:16:38 +0100

    [ UPDATE ] new targets added for testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:3001",
+        /*target: "http://pserver.mckay-projects.com",*/
+        /*target: "http://84.3.230.1:3001",*/
         changeOrigin: true,
         secure: false,
       },
