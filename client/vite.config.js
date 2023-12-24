7f335c75c7a58190764262b5e8517537687196a3
McKay <kg89szeged@gmail.com>
2023-12-24 14:29:32 +0100

    [ UPDATE ] notification added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/* INFO
+  - Kirúgásnál is törölje a socket ID-t a tömbből !!
+*/
+
 /* CONFIG */
+// const serverAddress = "https://3.253.24.47:3001";
 const serverAddress = "http://localhost:3001";
 
 export default defineConfig({
